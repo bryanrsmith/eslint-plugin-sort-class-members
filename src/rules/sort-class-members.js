bdb9cd2593117a93a49d3f5dc9bda982c225d02e
omfdy <oldmonkfromdesert@gmail.com>
2022-12-01 11:33:43 +0530

    supporting existing condition plus one more check

--- src/rules/sort-class-members.js
+++ src/rules/sort-class-members.js
@@ -170,7 +170,10 @@ function getClassMemberInfos(classDeclaration, sourceCode, orderedSlots) {
 
 function getMemberInfo(node, sourceCode) {
 	node.accessibility = node.accessibility || 'public';
-	const isPrivate = node.accessibility ? node.accessibility === 'private': false;
+	const isPrivate =
+		node.key.type === 'PrivateName' ||
+		node.key.type === 'PrivateIdentifier' ||
+		node.accessibility === 'private';
 	let name;
 	let type;
 	let propertyType;
